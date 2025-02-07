b3e719dfe3420051e95c2100f53cafd985f57d88
ahmed <ahmed.marzougui@esprit.tn>
2025-02-07 20:41:16 +0100

    add employee

--- angularui/src/app/app.module.ts
+++ angularui/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './employee/update-employee/update-employee.component';
+import { ListEmployeeComponent } from './employee/list-employee/list-employee.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    ListEmployeeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+      HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
